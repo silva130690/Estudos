67f2f1028e2efb9be1caca9ab24194ce84bd9025
silva130690 <alex13_suporte@hotmail.com>
2019-01-18 00:28:33 -0300

    Implementacao Dao
    Criando tabela e campos.

--- cursomc/src/main/java/com/alex/silva/cursomc/domain/Categoria.java
+++ cursomc/src/main/java/com/alex/silva/cursomc/domain/Categoria.java
@@ -2,9 +2,26 @@ package com.alex.silva.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+/*Autor Alex Silva
+ *mail:
+ *Created date: 18/01/19
+ *Descritions: Entidade do banco de dados.
+ */
+
+
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
